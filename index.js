dc09d0e0e4d775e9f6c200c61ccbb5112f40f8b3
Batuhan Yenidunya <batuhan.yenidunya@getir.com>
2022-04-17 22:46:07 +0300

    convert to express app

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
+const express = require("express");
 const axios = require('axios');
+const cacheService = require("express-api-cache");
 const FTP_URL = "https://www.python.org/ftp/python/";
 const REGEX = /(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?/gm
 
+// Initialize Express
+const app = express();
 
-export default async function handler(request, response) {
-	const res = await axios.get(FTP_URL);
-
-	const data = res.data.trim().replace(/\s+/g, '');
+// Create GET request
+app.get("/", cacheService.cache("1 day"), async (req, res) => {
+	const response = await axios.get(FTP_URL);
+	const data = response.data.trim().replace(/\s+/g, '');
 
 	const allVersions = [...new Set(Array.from(data.matchAll(REGEX), m => m[0]))];
+
 	const sortedAllVersions = allVersions.map(a => a.split('.').map(n => +n + 100000).join('.')).sort().map(a => a.split('.').map(n => +n - 100000).join('.'));
-
 	const uniq = new Set(sortedAllVersions);
+	
+	res.send([...uniq]);
+});
 
-	response.status(200).json({
-		versions: uniq
-	});
-}
+// Initialize server
+app.listen(5001, () => {
+	console.log("Running on port 5001.");
+});
+
+module.exports = app;
